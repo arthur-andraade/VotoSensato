e9ba641e5ce1437ff4ef4c6b9a0461dd9288a43f
arthur-andraade <arthurandradenf@gmail.com>
2020-06-10 19:16:09 -0300

    Frontend inicial em Vue.js

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <pesquisa/>
+  </div>
+</template>
+
+<script>
+import pesquisa from './components/Pesquisa.vue'
+
+export default {
+  name: 'App',
+  components: {
+    pesquisa
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
