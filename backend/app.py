238d901299aae195bd6193230ad847446723b7a0
arthur-andraade <arthurandradenf@gmail.com>
2020-06-18 17:23:03 -0300

    Implementando rota para fornecer dados de acorco com partido selecionado

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import jsonify as json
-from ufs import ColetadorUfs
 from partidos import ColetadorPartidos
 from coletador import Coletador
 app = Flask(__name__)
@@ -9,49 +8,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def dadosParaPesquisa():
     
-    coletandoUfs = ColetadorUfs()
     coletandoPartidos = ColetadorPartidos()
     
     response = json({
-        "ufs": coletandoUfs.getUfs(),
         "partidos": coletandoPartidos.getPartidos()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-# Rota composta por partido e estado
-@app.route('/pesquisandoDeputados')
-def coletandoDeputadoPorEstadoEParido():
-    pass
-
 # Rota partido
-@app.route('/partido/<siglaPartido>')
-def rotaPartido(siglaPartido):
+@app.route('/partido/<idPartido>')
+def rotaPartido(idPartido):
     
-    dadosPartido = Coletador().coletaPartido(siglaPartido)
-    
-    return json(
+    dadosPartido = ColetadorPartidos()
+    response = json(
         {
-            "messagem": "Acessando rota de partidos",
-            "de": "Admin",
-            "partidoDados": dadosPartido
+            "dados": dadosPartido.getDadosPartido(idPartido)
         }
     )
-
-# Rota estado
-@app.route('/estado/<siglaEstado>')
-def rotaEstado(siglaEstado):
-    
-    dadosEstado = Coletador().coletaEstado(siglaEstado)
-    
-    return json(
-        {
-            "messagem": "Acessando rota de estado",
-            "de": "Admin",
-            "estadoDados": dadosEstado
-        }
-    )
-
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='8000', debug=True)
--- backend/partidos.py
+++ backend/partidos.py
@@ -7,7 +7,42 @@ class ColetadorPartidos():
         self.partidosFormatados = []
         
         for partido in self.partidos['dados']:
-            self.partidosFormatados.append(partido['sigla'])
-        
+            dictPartido = {
+                'id': 0, 
+                'sigla': ' '
+            }
+            dictPartido['id'] = partido['id']
+            dictPartido['sigla'] = partido['sigla']
+            
+            self.partidosFormatados.append(dictPartido)
+
     def getPartidos(self):
         return self.partidosFormatados
+    
+    def getDadosPartido(self, idPartido):
+        url = 'https://dadosabertos.camara.leg.br/api/v2/partidos/{0}'.format(idPartido)
+        # Pegando dados sobre o PARTIDO
+        dadosPartido = requests.get(url).json()['dados']
+        dadosPartidoFormatado = {}
+        dadosPartidoFormatado['sigla'] = dadosPartido['sigla']
+        dadosPartidoFormatado['nome'] = dadosPartido['nome']
+        dadosPartidoFormatado['logo'] = dadosPartido['urlLogo']
+        dadosPartidoFormatado['totalPosse'] = dadosPartido['status']['totalPosse']
+        dadosPartidoFormatado['totalMembros'] = dadosPartido['status']['totalMembros']
+        dadosPartidoFormatado['situacao'] = dadosPartido['status']['situacao']
+        
+        # Pegando dados sobre MEMBROS do PARTIDO
+        url += '/membros'
+        dadosMembrosPartido = requests.get(url).json()['dados'] 
+        dadosMembrosPartidoFormatado = []
+        for membro in dadosMembrosPartido:
+            dadosMembro = {}
+            dadosMembro['id'] = membro['id']
+            dadosMembro['nome'] = membro['nome']
+            dadosMembro['uf'] = membro['siglaUf']
+            dadosMembro['image'] = membro['urlFoto']
+            dadosMembro['email'] = membro['email']
+            dadosMembrosPartidoFormatado.append(dadosMembro)
+        
+        dadosPartidoFormatado['membros'] = dadosMembrosPartidoFormatado
+        return dadosPartidoFormatado
