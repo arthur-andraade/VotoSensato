ddf6163d4f56baa6177763909bb9ab655a865bdb
arthur-andraade <arthurandradenf@gmail.com>
2020-06-18 19:56:22 -0300

    Função que faz request na API e recebe os dados do partido

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -2,12 +2,21 @@ import acessandoApi from './config';
 
 const api = {
     
+    // Pegando parâmetros na API para realizar pesquisa
     parametrosIniciais: async () => {
         const parametrosIniciais = await acessandoApi.get('/').then(
             res => res.data
         );
         return parametrosIniciais;
-    }
+    },
+
+    // Pegando dados específico de PARTIDO
+    dadosPartido: async (idPartido) =>{
+        const dados = await acessandoApi.get(`/partido/${idPartido}`).then(
+            res => res.data
+        )
+        return dados;
+    }   
 }
 
 export default api;
