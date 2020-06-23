5d574a6c3d2c8b132ead07c7dca532b0919fd485
arthur-andraade <arthurandradenf@gmail.com>
2020-06-23 17:53:00 -0300

    Adicionando rota para aplicar filtro

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask import jsonify as json
 from partidos import ColetadorPartidos
-from coletador import Coletador
+from ufs import ColetadorUfs
+from flask import request
 app = Flask(__name__)
 
 # Rota principal
@@ -28,6 +29,29 @@ def rotaPartido(idPartido):
     )
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
+@app.route('/ufs')
+def rotaUfs():
+    coletandoUfs = ColetadorUfs()
+    response = json({
+        "siglas": coletandoUfs.getUfs()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/filtro')
+def rotaFiltro():
+    partido = request.args.get('partido')
+    uf = request.args.get('uf')
+    filtro = ColetadorPartidos()
+    
+    response = json({
+        "membrosFiltrado": filtro.getDeputadoPorEstado(uf, partido)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port='8000', debug=True)
